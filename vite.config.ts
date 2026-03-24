7307256fd5e421a7de34d7e1a7e50477b6644023
Arnav Mana <2arnavmana@gmail.com>
2026-03-23 20:01:08 -0700

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
+import { tanstackStart } from '@tanstack/start/vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   plugins: [
+    tanstackStart(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart(),
-    viteReact(),
   ],
 })
 
